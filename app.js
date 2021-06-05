2369b1f0404e815a2aaee0b1cd955e5bf4b17e24
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-05 22:18:10 +0530

    function invoked without export

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 // CommonJS, every file is module {by default}
 const names = require('./4-names');
 const sayHi = require('./5-utils')
-console.log(names);
+const data = require('./6-alternative-flavour')
+require('./7-mind-grenade')
 sayHi('susan')
 sayHi(names.john)
 sayHi(names.peter)
